d4a87e0b07536598dd5ab1074c0e9bbd3f171b65
gyuhyeongH <ckacl792@gmail.com>
2022-05-27 18:10:47 +0900

    3단계

--- timeattack3/views.py
+++ timeattack3/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import hashlib
 
 def first_view(request):
     return render(request, 'index.html')
@@ -14,5 +14,11 @@ def sign_in_view(request):
     if request.method == 'POST':
         user=User()
         user.email=request.POST.get('email', None)
-        user.password=request.POST.get('password', None)
-        return render(request, 'index.html')
+        password=request.POST.get('password', None)
+        hashed_pw = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        user.password=hashed_pw
+        if len(user.password) <8:
+            return HttpResponse("비밀번호 형식 에러")
+        if '@' not in user.email:
+            return HttpResponse("이메일 형식 에러")
+        return render(request, 'register.html')
